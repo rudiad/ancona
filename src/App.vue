e5515ad1ad6d4db1438a060babf63da4e27e7333
Andy Rudi <rudiad@gmail.com>
2023-07-09 14:42:32 -0400

    Vite initial

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Walter+Turncoat" rel="stylesheet" type="text/css" />
+
+<template>
+    
+  <div class="header">
+      <div class="logo-group">
+          <!-- <img class="logo" src="./assets/ancona-logo-image-trace.png" /> -->
+          <!-- <img class="logo" src="./assets/ancona-logo-image-trace-ver2.png" /> -->
+          <!-- <img class="logo" src="./assets/ancona-logo-image-trace-ver3.png" /> -->
+          <!-- <img class="logo" src="./assets/ancona-logo-image-trace-ver4.png" /> -->
+          <!-- <img class="logo" src="./assets/ancona-logo-image-trace-ver5.png" /> -->
+          <!-- <img class="logo" src="./assets/ancona-logo-image-trace-ver3.png" /> -->
+          <img class="logo" src="./assets/ancona-logo-image-trace-ver8.png" />
+
+          <div class="title">Ancona Montesorri School</div>
+      </div>
+      <div class="contact-group">
+          <img class="contact-icon" src="./assets/contact-icon.png" />
+          <div class="contact">Contact Staff</div>
+      </div>
+  </div>
+
+  <div class="navigation">
+      <a href="">Home</a> <span class="divider">|</span> <a href="">About</a> <span class="divider">|</span> <a href="">Classes</a> <span class="divider">|</span> <a href="">Staff</a> <span class="divider">|</span> <a href="">Enrollment</a> <span class="divider">|</span> <a href="">Information</a>
+  </div>
+
+  <div class="mission-statement">
+      <span class="mission-span">Ancona Montessori School</span> is proud to invest in YOUR child. We provide your children with many life skills and a solid foundation of educational principals. Ancona encourages children to be independent, healthy, and confident. Come visit us to learn more about how Ancona Schools can provide your child with the very best in early childhood development services.
+  </div>
+
+  <div class="content">        
+
+      <div class="container">
+          <div class="col col-1">
+              <h2>About us</h2>
+
+              Montessori School in Drexel Hill and Wynnewood, Pennsylvania
+              <a href="">READ MORE</a>
+
+              <h2>Hours Of Operation:</h2>
+
+              Monday-Friday: 7:30AM-6:00PM
+          </div>
+
+          <div class="col col-2">
+              <h2>Address</h2>
+
+              Drexel Hill: 5290 Townshipline Road, 19026
+
+              Wynnewood: 630 Clothier Road, 19096
+
+              484-431-6469
+              <a href="">CONTACT</a>
+          </div>
+          
+          <div class="col col-3">
+              <h2>Photo Gallery</h2>
+              <img class="gallery-still" src="./assets/gallery-still.jpg" />
+              <a href="">SEE GALLERY</a>
+          </div>
+      </div>
+
+  </div>
+
+  <div class="footer">
+      <div class="footer-links">
+          <a href="https://mybrightwheel.com"><img src="./assets/custom_avatar-1626993963.png" />Brightwheel</a>
+          <span class="divider">|</span>
+          <a href="">Directions</a>
+          <span class="divider">|</span>
+          <a href="">FAQs</a>
+      </div>
+      <div class="copyright">Copyright © 2023 Ancona Montesorri School. All Rights Reserved.</div>
+  </div>
+    
+</template>
+
+<style>
+</style>
